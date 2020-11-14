d66272cc122201257d80dd78c91fd1c7df39ed67
Utai <74049768+UtaiC@users.noreply.github.com>
2020-11-14 13:20:01 +0700

    Update SIM-STOCK-ST.py

--- SIM-STOCK-ST.py
+++ SIM-STOCK-ST.py
@@ -159,7 +159,7 @@ FG1stock=(FGbl-Delis)
 
 Showstock=STbf[['BF_STOCK','BM_STOCK','FG0_STOCK','FG1_STOCK','Delivery']]
 #howstock.to_excel('STOCKUPDATE.xlsx')
-st.subheader('STOCK DATA after Balnace')
+st.subheader('Ending STOCK')
 st.write(Showstock)
 st.bar_chart(Showstock)
 
