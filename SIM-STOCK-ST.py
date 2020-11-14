f43fad4c5d4bef348a03cdc3a59b7c2c7ecfe9b6
Utai <74049768+UtaiC@users.noreply.github.com>
2020-11-14 13:07:36 +0700

    Update SIM-STOCK-ST.py

--- SIM-STOCK-ST.py
+++ SIM-STOCK-ST.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn import datasets
+#from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
